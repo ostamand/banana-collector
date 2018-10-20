9c3e8d576f794044333fc9fe992f1e49221eb2a3
O1SA <olivier.st.amand.1@gmail.com>
2018-10-20 11:22:57 -0400

    fix: Missing env_file update

--- Visual/visual_env.py
+++ Visual/visual_env.py
@@ -3,8 +3,6 @@ from unityagents import UnityEnvironment
 import numpy as np
 from collections import deque
 
-STATE_SIZE = (84,84)
-
 class VisualEnvironment():
 
     def __init__(self, env_file, state_stack=4, train=True):
@@ -18,8 +16,8 @@ class VisualEnvironment():
         
     def step(self, action):
         env_info = self.env.step(action)[self.brain_name]
-        self.states_history.append(self.preprocess(env_info.visual_observations[0]))
-        next_state = np.array(self.states_history).reshape((1, self.state_stack, *STATE_SIZE))
+        self.states_history.append(env_info.visual_observations[0].transpose([0,3,1,2]))
+        next_state = np.array(self.states_history).transpose([1,2,0,3,4])
         reward = env_info.rewards[0]
         done = env_info.local_done[0]
         return (next_state, reward, done)
@@ -27,18 +25,15 @@ class VisualEnvironment():
     def sample(self):
         return np.random.randint(0, self.action_size)
 
+    # batch_size, channels (RGB), depth (state_stack), height, width
     def reset(self, train=True):
         self.states_history = deque(maxlen=self.state_stack)
         for _ in range(self.state_stack):
-            self.states_history.append(np.zeros(STATE_SIZE))
+            self.states_history.append(np.zeros((1,3,84,84)))
         # Reset environment 
         env_info = self.env.reset(train_mode=train)[self.brain_name]
-        self.states_history.append(self.preprocess(env_info.visual_observations[0]))
-        return np.array(self.states_history).reshape((1, self.state_stack, *STATE_SIZE))
-
-    def preprocess(self, state_rgb):
-        gray = 0.2989 * state_rgb[0,:,:,0] + 0.5870 * state_rgb[0,:,:,1] + 0.1140 * state_rgb[0,:,:,2]
-        return gray
+        self.states_history.append(env_info.visual_observations[0].transpose([0,3,1,2]))
+        return np.array(self.states_history).transpose([1,2,0,3,4])
 
     def close(self):
         self.env.close()
